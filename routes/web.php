b63f516461f4ebf141588439e92a0cf8650f8e8c
= <=>
2020-04-14 10:40:48 +0700

    merge brand

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,17 @@ Route::get('admin-logout', 'Admin\HomeController@logout')->name('adminlogout');
 
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'adminlogin'], function () {
     //Route::get('dashboard', 'Dashboard@index');
-    Route::get('dashboard','HomeController@index')->name('dashboard');
-    
+    Route::get('dashboard', 'HomeController@index')->name('dashboard');
+
     //Banner
-    Route::get('/add-banner','BannerController@add_banner')->name('add-banner');
-    Route::post('/add-banner','BannerController@post_add_banner')->name('post_add_banner');
-    Route::get('/all-banner','BannerController@all_banner')->name('all-banner');
-    Route::get('/sta/{id}','BannerController@active_banner')->name('active_banner');
-    Route::get('/sta1/{id}','BannerController@unactive_banner')->name('unactive_banner');
-    Route::get('/edit-banner/{id}','BannerController@edit_banner')->name('edit-banner');
-    Route::post('/edit-banner/{id}','BannerController@post_edit_banner')->name('edit-banner');
-    Route::get('/delete-banner/{id}','BannerController@delete_banner')->name('delete-banner');
+    Route::get('/add-banner', 'BannerController@add_banner')->name('add-banner');
+    Route::post('/add-banner', 'BannerController@post_add_banner')->name('post_add_banner');
+    Route::get('/all-banner', 'BannerController@all_banner')->name('all-banner');
+    Route::get('/sta/{id}', 'BannerController@active_banner')->name('active_banner');
+    Route::get('/sta1/{id}', 'BannerController@unactive_banner')->name('unactive_banner');
+    Route::get('/edit-banner/{id}', 'BannerController@edit_banner')->name('edit-banner');
+    Route::post('/edit-banner/{id}', 'BannerController@post_edit_banner')->name('edit-banner');
+    Route::get('/delete-banner/{id}', 'BannerController@delete_banner')->name('delete-banner');
     Route::get('/', function () {
         return redirect(route('dashboard'));
     });
@@ -49,28 +49,29 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admi
 
         //show list product
         Route::get('list-product', 'ProductController@getListProduct')->name('listProduct');
-        Route::get('edit-product/{id}','ProductController@getEditProduct')->name('editProduct');
-        Route::get('del-product/{id}','ProductController@delProduct')->name('delProduct');
-        Route::get('update-product/{id}','ProductController@updateProduct')->name('updateProduct');
-        Route::get('update-info-product/{id}','ProductController@updateInfoProduct')->name('updateInfoProduct');
+        Route::get('edit-product/{id}', 'ProductController@getEditProduct')->name('editProduct');
+        Route::get('del-product/{id}', 'ProductController@delProduct')->name('delProduct');
+        Route::get('update-product/{id}', 'ProductController@updateProduct')->name('updateProduct');
+        Route::get('update-info-product/{id}', 'ProductController@updateInfoProduct')->name('updateInfoProduct');
 
         //edit image product
         Route::get('image-product/{id}', 'ProductController@getImageProduct')->name('imageProduct');
-        Route::post('add-image-product/{id}','ProductController@addImageProuct')->name('addImageProduct');
+        Route::post('add-image-product/{id}', 'ProductController@addImageProuct')->name('addImageProduct');
         Route::get('del-image-product/{id}', 'ProductController@delImageProduct')->name('delImageProduct');
-        Route::get('update-image-product/{id}/{status}','ProductController@updateTmageProduct')->name('updateImageProduct');
+        Route::get('update-image-product/{id}/{status}', 'ProductController@updateTmageProduct')->name('updateImageProduct');
 
         //change avatar
-        Route::get('change-avatar/{image_id}/{products_id}','ProductController@changeAvatar')->name('changeAvatar');
-        
+        Route::get('change-avatar/{image_id}/{products_id}', 'ProductController@changeAvatar')->name('changeAvatar');
     });
+
+    // brand: add, edit/update , delete
     Route::group(['prefix' => 'brand'], function () {
-        Route::get('add','BrandController@getAddBrand')->name('addBrand');
-        Route::post('post','BrandController@postBrand')->name('postBrand');
-        Route::get('list','BrandController@getListBrand')->name('listBrand');
-        Route::get('edit/{id}','BrandController@getEditBrand')->name('editBrand');
-        Route::get('update/{id}','BrandController@updateBrand')->name('updateBrand');
-        Route::get('delete/{id}','BrandController@delBrand')->name('deleteBrand');
+        Route::get('add', 'BrandController@getAddBrand')->name('addBrand');
+        Route::post('post', 'BrandController@postBrand')->name('postBrand');
+        Route::get('list', 'BrandController@getListBrand')->name('listBrand');
+        Route::get('edit/{id}', 'BrandController@getEditBrand')->name('editBrand');
+        Route::get('update/{id}', 'BrandController@updateBrand')->name('updateBrand');
+        Route::get('delete/{id}', 'BrandController@delBrand')->name('deleteBrand');
     });
 });
 
